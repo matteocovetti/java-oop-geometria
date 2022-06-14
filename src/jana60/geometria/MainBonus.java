d462f2e7e3f7a8d858e735a111409a6986156924
matteocovetti <covetti.matteo@gmail.com>
2022-06-14 15:53:35 +0200

    bonus finito

--- src/jana60/geometria/MainBonus.java
+++ src/jana60/geometria/MainBonus.java
@@ -1,5 +1,7 @@
 package jana60.geometria;
 
+import java.util.Scanner;
+
 /*chiedere all’utente per 5 volte di definire le dimensioni di un rettangolo
  *e stampare a video l’area del rettangolo più grande
  */
@@ -7,7 +9,42 @@ package jana60.geometria;
 public class MainBonus {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Rettangolo rettangolo1 = new Rettangolo();
+
+		int areaMaggiore = 0;
+
+		// aggiungo scanner
+		Scanner scan = new Scanner(System.in);
+
+		for (int i = 0; i < 5; i++) {
+			System.out.print("Inserisci base del rettangolo numero " + (i + 1) + ":");
+			rettangolo1.baseRettangolo = scan.nextInt();
+
+			System.out.print("Inserisci altezza del rettangolo numero " + (i + 1) + ":");
+			rettangolo1.altezzaRettangolo = scan.nextInt();
+
+			if (rettangolo1.areaRettangolo() >= areaMaggiore) {
+				areaMaggiore = rettangolo1.areaRettangolo();
+			}
+		}
+
+		System.out.println("L'area maggiore è: " + areaMaggiore);
+
+		// chiedo di inserire base
+//		System.out.print("Inserisci il valore della base del rettangolo: ");
+//		rettangolo1.baseRettangolo = scan.nextInt();
+
+		// chiedo di inserire altezza
+//		System.out.print("Inserisci il valore dell'altezza del rettangolo: ");
+//		rettangolo1.altezzaRettangolo = scan.nextInt();
+
+		// stampo perimetro
+//		rettangolo1.stampaPerimetro();
+
+		// stampo area
+//		rettangolo1.stampaArea();
+
+		scan.close();
 
 	}
 
