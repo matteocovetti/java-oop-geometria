69140c87016ced04f595d671bc9c98279604b66f
matteocovetti <covetti.matteo@gmail.com>
2022-06-14 14:41:32 +0200

    add attributi-metodi

--- src/jana60/geometria/Main.java
+++ src/jana60/geometria/Main.java
@@ -1,5 +1,10 @@
 package jana60.geometria;
 
+/* una classe Main contenente il metodo main, nel quale chiedete all’utente di inserire, da console,
+ * i valori di base e di altezza con cui istanziare un nuovo oggetto Rettangolo.
+ * Dopo averlo istanziato, stampate a video il perimetro e l’area.
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
--- src/jana60/geometria/Rettangolo.java
+++ src/jana60/geometria/Rettangolo.java
@@ -1,5 +1,21 @@
 package jana60.geometria;
 
-public class Rettangolo {
+/*una classe Rettangolo con due attributi interi: base e altezza. 
+ *La classe ha due metodi che calcolano e restituiscono, rispettivamente,
+ *l’area e il perimetro del rettangolo.
+ */
 
+public class Rettangolo {
+	// attributi
+	int base;
+	int altezza;
+
+	// metodi
+	int areaRettangolo() {
+		return base * altezza;
+	}
+
+	int perimetroRettangolo() {
+		return (base + altezza) * 2;
+	}
 }
