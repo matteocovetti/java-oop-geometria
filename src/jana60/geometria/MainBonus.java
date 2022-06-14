8a5f0478c68628e7f1a176dfdb9bc52d5407e718
matteocovetti <covetti.matteo@gmail.com>
2022-06-14 15:35:40 +0200

    inizio bonus

---
+++ src/jana60/geometria/MainBonus.java
@@ -0,0 +1,14 @@
+package jana60.geometria;
+
+/*chiedere all’utente per 5 volte di definire le dimensioni di un rettangolo
+ *e stampare a video l’area del rettangolo più grande
+ */
+
+public class MainBonus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
